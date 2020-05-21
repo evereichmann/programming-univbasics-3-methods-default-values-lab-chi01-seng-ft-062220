bb8bc375a2de2c58eaaffb6404a71eae3c289272
Eve Reichmann <ereichmann4@aol.com>
2020-05-21 19:06:54 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,9 @@
 def meal_choice(veg1, veg2, protien)
   puts "What a butritious meal!"
   puts "A plate of #{protien} with #{veg1} and #{veg2}."
+  meal="A plate of #{protien} with #{veg1} and #{veg2}."
+  puts meal
+  meal
 end
 
 meal_choice("carrots","string beans", "lentils")
@@ -8,4 +11,9 @@ meal_choice("carrots","string beans", "lentils")
 def meal_choice(veg1, veg2, protien= "tofu")
   puts "What a butritious meal!"
   puts "A plate of #{protien} with #{veg1} and #{veg2}."
+  meal="A plate of #{protien} with #{veg1} and #{veg2}."
+  puts meal
+  meal
 end
+
+meal_choice("carrots", "string beans")
